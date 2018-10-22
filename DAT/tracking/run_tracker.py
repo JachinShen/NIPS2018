dbcdfc3dee59bbdb028ea4953423a6b6ad4fa8f5
JachinShen <jachinshen@foxmail.com>
2018-10-22 19:31:08 +0800

    change cuda visible devices to 1

--- DAT/tracking/run_tracker.py
+++ DAT/tracking/run_tracker.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 import sys
 import time
 import argparse
