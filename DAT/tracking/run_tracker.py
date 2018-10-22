3cbaae1b1c127344e6b1b464452b5889b1aba5b0
JachinShen <jachinshen@foxmail.com>
2018-10-22 19:36:21 +0800

    change cuda visible devices to 0

--- DAT/tracking/run_tracker.py
+++ DAT/tracking/run_tracker.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import sys
 import time
 import argparse
